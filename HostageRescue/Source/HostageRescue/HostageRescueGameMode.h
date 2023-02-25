f0387201aa30cb7ffd595fe9071664e1aa5b9ac3
JoshuaOliver99 <joshuaoliversimpson@gmail.com>
2023-02-25 10:24:26 +0000

    Project Init

---
+++ HostageRescue/Source/HostageRescue/HostageRescueGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HostageRescueGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHostageRescueGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHostageRescueGameMode();
+};
+
+
+
